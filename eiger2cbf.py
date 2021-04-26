0f12e02a06a21313b1a104785294b3616cdb182f
Jun Aishima <jaishima@bnl.gov>
2021-04-26 14:55:51 -0400

    add handling of rows, testing config
     * add ability to input sweeps
     * add active_only flag - if flag is set, will use non-active requests
     * remove hard-coded seqNum

--- eiger2cbf.py
+++ eiger2cbf.py
@@ -1,27 +1,42 @@
 #$/usr/bin/env python3
+#eiger2cbf currently designed for rastering rows. could be more general
+# arguments:
+# collection_id - uuid
+# start_index - first image of hdf5 file to convert
+# end_index - last image of hdf5 file to convert
+# sweep_start - set to 0 if not part of a row
+# seq_num - collection sequence number
+
 import os
+import sys
 import os.path
 import db_lib
 from daq_utils import getBlConfig
 
-collection_id, start_index, end_index, seq_num = sys.argv[0:4]
+collection_id, start_index, end_index, sweep_start, seq_num = sys.argv[1:6]
 start_index = int(start_index)
 end_index = int(end_index)
 seq_num = int(seq_num)
-cbf_comm = getBlConfig('cbf_comm')
-
-request = db_lib.getRequestByID(collection_id)
+cbf_comm = getBlConfig('cbfComm')
+if len(sys.argv) > 6:
+    active_only = False
+else:
+    active_only = True
+request = db_lib.getRequestByID(collection_id, active_only)
 directory = request["request_obj"]["directory"]
 prefix = request["request_obj"]["file_prefix"]
-seq_num = 100#request[] #not stored at the moment
 row_cell_count = request["request_obj"]["rasterDef"]["rowDefs"][0]["numsteps"]
 
 hdf_sample_data_pattern = os.path.join(directory, f'{prefix}_')
 hdf_row_file_pattern = f'{hdf_sample_data_pattern}{int(float(seq_num))}_master.h5'
-cbf_conversion_pattern = os.path.join(cbfDir, f'{prefix}_{row_index}_')
+cbf_dir = os.path.join(directory, 'cbf')
+if sweep_start > 0:
+    cbf_conversion_pattern = os.path.join(cbf_dir, f'{prefix}_{sweep_start}_')
+else:
+    cbf_conversion_pattern = os.path.join(cbf_dir, f'{prefix}_')
 cbf_pattern = cbf_conversion_pattern + "*.cbf"
 
 print(directory, prefix, row_cell_count, hdf_sample_data_pattern, hdf_row_file_pattern, cbf_conversion_pattern, cbf_pattern, start_index, end_index)
-os.makedirs(os.path.join(directory, 'cbf'))
+os.makedirs(cbr_dir)
 command_string = "{cbf_comm} {hdf_row_file_pattern} {start_index}:{end_index} {cbf_conversion_pattern}"
 os.system(command_string)
--- eiger2cbf.sh
+++ eiger2cbf.sh
@@ -1,3 +1,3 @@
 #$/bin/bash
 conda activate lsdcServer_2020-1.0
-python eiger2cbf.py $1 $2 $3
+python eiger2cbf.py $1 $2 $3 $4 $5
