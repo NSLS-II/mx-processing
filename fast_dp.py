2243e2c39d5851e8425b43d8375a344842816edd
Jun Aishima <jaishima@bnl.gov>
2021-04-28 09:40:01 -0400

    use WRAPPERSDIR

--- fast_dp.py
+++ fast_dp.py
@@ -16,7 +16,7 @@ running_dir = os.path.join(directory, 'fastDPOutput')
 file_prefix = request["request_obj"]["file_prefix"]
 prefix_long = os.path.join(directory, f'{file_prefix}_{seq_num}')
 hdf_file_pattern = f'{prefix_long}_master.h5'
-fast_dp_comm = f'source {os.environ["PROJDIR"]}wrappers/fastDPWrap2;{getBlConfig("fastdpComm")}'
+fast_dp_comm = f'source {os.environ["WRAPPERSDIR"]}fastDPWrap2;{getBlConfig("fastdpComm")}'
 comm_s = f"cd {running_dir};{fast_dp_comm} {hdf_file_pattern}"
 logger.info(f'Fast DP invocation: {comm_s}')
 os.system(comm_s)
