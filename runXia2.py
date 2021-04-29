e1189096e791a8da503366d817f8638aae17f138
Jun Aishima <jaishima@bnl.gov>
2021-04-29 15:10:58 -0400

    remove stuff we don't need to call updated runXia2.py

--- runXia2.py
+++ runXia2.py
@@ -1,26 +1,46 @@
 #!/usr/bin/python
 import time
 import os
+import os.path
 import sys
 import db_lib
 import json
 import daq_utils
 
+def create_filename(prefix,number):
+  if (detector_id == "EIGER-16"):
+   tmp_filename = findH5FirstData(prefix)
+  else:
+    tmp_filename = "%s_%05d.cbf" % (prefix,int(number))
+  if (prefix[0] != "/"):
+    cwd = os.getcwd()
+    filename = "%s/%s" % (cwd,tmp_filename)
+  else:
+    filename = tmp_filename
+  return filename
 
-directory = sys.argv[1]
-runningDir = directory+"/xia2Output"
-comm_s = "mkdir -p " + runningDir
-os.system(comm_s)
+def findH5FirstData(prefix):
+  comm_s = "ls " + prefix + "*_data_000001.h5"
+  firstDataFilename = os.popen(comm_s).read()[0:-1]
+  return firstDataFilename
+
+request_id = sys.argv[1]
+request = db_lib.getRequestFromID(request_id)
+directory = request["directory"]
+runningDir = os.path.join(directory, "xia2Output")
+os.makedirs(runningDir)
 os.chdir(runningDir)
 
-filePrefix = sys.argv[2]
-numstart = int(sys.argv[3])
-numimages = int(sys.argv[4])
-request_id = int(sys.argv[5])
+filePrefix = request["prefix"]
+numstart = int(request["file_number_start"])
+sweep_end = float(request["sweep_end"])
+sweep_start = float(request["sweep_start"])
+img_width = float(request["img_width"])
+numimages = round(abs(sweep_end-sweep_start)/img_width)
 
 expectedFilenameList = []
 timeoutLimit = 60 #for now
-prefix_long = directory+"/"+filePrefix
+prefix_long = os.path.join(directory, filePrefix)
 for i in range (numstart,numstart+numimages):
   filename = daq_utils.create_filename(prefix_long,i)
   expectedFilenameList.append(filename)
@@ -33,14 +53,8 @@ while(not os.path.exists(expectedFilenameList[len(expectedFilenameList)-1])): #t
 comm_s = "xia2 " + directory
 print(comm_s)
 os.system(comm_s)
-fd = open("xia2.json")
-resultObj = json.loads(fd.read())
-fd.close()
+with open("xia2.json") as fd:
+    resultObj = json.loads(fd.read())
 print(resultObj)
 db_lib.addResultforRequest("xia2",request_id,resultObj)
 print("finished xia2")
-
-
-
-
-
--- xia2.py
+++ xia2.py
@@ -5,23 +5,7 @@
 
 import os
 import sys
-import os.path
-import db_lib
-from daq_utils import getBlConfig
 
 collection_id = sys.argv[1:2]
-if len(sys.argv) > 1:
-    active_only = False
-else:
-    active_only = True
-
-request = db_lib.getRequestByID(collection_id, active_only)
-directory = request["request_obj"]["directory"]
-prefix = request["request_obj"]["prefix"]
-sweep_start = float(request["request_obj"]["sweep_start"])
-sweep_end = float(request["request_obj"]["sweep_end"])
-img_width = float(request["request_obj"]["img_width"])
-num_images = round(abs(sweep_end - sweep_start) / img_width)
-file_number_start = request["request_obj"]["file_number_start"]
-comm_s = f"{os.environ['LSDCHOME']}/runXia2.py {directory} {prefix} {file_number_start} {num_images} {collection_id}"
+comm_s = f"runXia2.py {collection_id}"
 os.system(comm_s)
