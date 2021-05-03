f70267445fa9d3cc332e79fb017db87dc378390a
Jun Aishima <jaishima@bnl.gov>
2021-05-03 18:38:23 -0400

    use beamline from request, fix up details of call
     * get correct fast DP call
     * get desired values in commandline arguments from
       test ssh calls - fix indices
     * remove unnecessary hash-bang - use python
       directly from conda env instead

--- fast_dp.py
+++ fast_dp.py
@@ -1,22 +1,23 @@
-#$/usr/bin/env python3
 import os
 import sys
 import os.path
 import db_lib
 from daq_utils import getBlConfig
 
-collection_id, seq_num = sys.argv[2:4]
-if len(sys.argv) > 4:
+collection_id, seq_num = sys.argv[1:3]
+if len(sys.argv) > 3:
     only_active_proc = False
 else:
     only_active_proc = True
+
 result = db_lib.getRequestByID(collection_id, only_active_proc)
+beamline = result['beamline']
 directory = result["request_obj"]["directory"]
 running_dir = os.path.join(directory, 'fastDPOutput')
 file_prefix = result["request_obj"]["file_prefix"]
 prefix_long = os.path.join(directory, f'{file_prefix}_{seq_num}')
 hdf_file_pattern = f'{prefix_long}_master.h5'
-fast_dp_comm = f'source {os.environ["WRAPPERSDIR"]}/fastDPWrap2;{getBlConfig("fastdpComm")}'
+fast_dp_comm = f'source {os.environ["WRAPPERSDIR"]}/fastDPWrap2;{getBlConfig("fastdpComm", beamline)}'
 comm_s = f"cd {running_dir};{fast_dp_comm} {hdf_file_pattern}"
 print(f'Fast DP invocation: {comm_s}')
 os.system(comm_s)
