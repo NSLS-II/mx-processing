c95f156ccd969d8e4e46e327ee0108daec9f39af
Jun Aishima <jaishima@bnl.gov>
2021-05-03 15:47:31 -0400

    add more complete setup for environment

--- fast_dp.sh
+++ fast_dp.sh
@@ -1,3 +1,6 @@
 #$/bin/bash
 conda activate lsdcServer_2020-1.0
-python fast_dp.py "$@"
+export PROJDIR=/home/jaishima/code
+export PYTHONPATH=${PROJDIR}/lsdc:$PYTHONPATH
+export WRAPPERSDIR=$PROJDIR}/wrappers/
+python ${PROJDIR}/mx_processing/fast_dp.py "$@"
