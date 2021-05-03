7ef4573ad59e01a66286623c1d57fe55017e75ca
Jun Aishima <jaishima@bnl.gov>
2021-05-03 15:52:03 -0400

    fix directory name in production

--- fast_dp.sh
+++ fast_dp.sh
@@ -3,4 +3,4 @@ conda activate lsdcServer_2020-1.0
 export PROJDIR=/GPFS/CENTRAL/xf17id1/skinnerProjectsBackup/
 export PYTHONPATH=${PROJDIR}/lsdc:$PYTHONPATH
 export WRAPPERSDIR=$PROJDIR}/wrappers/
-python ${PROJDIR}/mx_processing/fast_dp.py "$@"
+python ${PROJDIR}/mx-processing/fast_dp.py "$@"
