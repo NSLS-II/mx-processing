254cff36d6e4a703f17816c5504802bbbd2d0a8c
Jun Aishima <jaishima@bnl.gov>
2021-05-03 14:52:06 -0400

    small fixes to get script runnable
     * fix variable name to result
     * use correct argv arguments
     * WRAPPERSDIR requires / afterwards

--- fast_dp.py
+++ fast_dp.py
@@ -5,18 +5,18 @@ import os.path
 import db_lib
 from daq_utils import getBlConfig
 
-collection_id, seq_num = sys.argv[1:3]
+collection_id, seq_num = sys.argv[2:4]
 if len(sys.argv) > 4:
     only_active_proc = False
 else:
     only_active_proc = True
 result = db_lib.getRequestByID(collection_id, only_active_proc)
-directory = request["request_obj"]["directory"]
+directory = result["request_obj"]["directory"]
 running_dir = os.path.join(directory, 'fastDPOutput')
-file_prefix = request["request_obj"]["file_prefix"]
+file_prefix = result["request_obj"]["file_prefix"]
 prefix_long = os.path.join(directory, f'{file_prefix}_{seq_num}')
 hdf_file_pattern = f'{prefix_long}_master.h5'
-fast_dp_comm = f'source {os.environ["WRAPPERSDIR"]}fastDPWrap2;{getBlConfig("fastdpComm")}'
+fast_dp_comm = f'source {os.environ["WRAPPERSDIR"]}/fastDPWrap2;{getBlConfig("fastdpComm")}'
 comm_s = f"cd {running_dir};{fast_dp_comm} {hdf_file_pattern}"
 print(f'Fast DP invocation: {comm_s}')
 os.system(comm_s)
