bafdb44b64c78a7da522732fabebfb6b5b50a267
Jun Aishima <jaishima@bnl.gov>
2021-05-03 15:50:08 -0400

    use location of project in production

--- fast_dp.sh
+++ fast_dp.sh
@@ -1,6 +1,6 @@
 #$/bin/bash
 conda activate lsdcServer_2020-1.0
-export PROJDIR=/home/jaishima/code
+export PROJDIR=/GPFS/CENTRAL/xf17id1/skinnerProjectsBackup/
 export PYTHONPATH=${PROJDIR}/lsdc:$PYTHONPATH
 export WRAPPERSDIR=$PROJDIR}/wrappers/
 python ${PROJDIR}/mx_processing/fast_dp.py "$@"
