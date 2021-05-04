b8e49c69d433c34a003df920c3f10d1db0a51b9f
Jun Aishima <jaishima@bnl.gov>
2021-05-04 14:57:06 -0400

    set up environment for cbf to jpeg processing

--- cbf2jpeg.sh
+++ cbf2jpeg.sh
@@ -1,3 +1,6 @@
 #$/bin/bash
-conda activate lsdcServer_2020-1.0
-python cbf2jpeg.py "$@"
+export PROJDIR=/GPFS/CENTRAL/xf17id1/skinnerProjectsBackup/
+export PYTHONPATH=${PROJDIR}/lsdc:$PYTHONPATH
+export WRAPPERSDIR=${PROJDIR}/wrappers/
+export BEAMLINE_ID='fmx' # TODO required to keep daq_utils happy
+/opt/conda_envs/mx-processing-env/bin/python ${PROJDIR}/mx-processing/cbf2jpeg.py "$@"
