79ac52ab39af5335ff2c26a366267cb698ca073e
Jun Aishima <jaishima@bnl.gov>
2021-05-05 11:54:32 -0400

    fix various issues with DIALS-calling script
     * get correct fields from request
     * get config values from correct beamline shown in
       request
     * fix flag and value names for DIALS spotfinding

--- dials_spotfind.py
+++ dials_spotfind.py
@@ -1,4 +1,3 @@
-#$/usr/bin/env python3
 #run dials spotfinding to process rastering rows
 # arguments:
 # collection_id - uuid
@@ -15,6 +14,7 @@ from config_params import * #RASTER_ parameters
 
 collection_id, row_index, seqNum = sys.argv[1:4]
 #TODO store beamline and detector so that we don't need to check seqNum
+seqNum = int(seqNum)
 row_index = int(row_index)
 if len(sys.argv) > 3:
     active_only = False
@@ -22,43 +22,41 @@ else:
     active_only = True
 
 request = db_lib.getRequestByID(collection_id, active_only)
-beamline = request['beamline']
+beamline = request['request_obj']['beamline']
 
-dials_comm = getBlConfig("dialsComm")
+dials_comm = getBlConfig("dialsComm", beamline)
 dials_tune_low_res = getBlConfig(RASTER_TUNE_LOW_RES, beamline)
 dials_tune_high_res = getBlConfig(RASTER_TUNE_HIGH_RES, beamline)
 dials_tune_ice_ring_flag = getBlConfig(RASTER_TUNE_ICE_RING_FLAG, beamline)
 dials_tune_reso_flag = getBlConfig(RASTER_TUNE_RESO_FLAG, beamline)
 dials_tune_thresh_flag = getBlConfig("rasterThreshFlag", beamline)    
-dials_tune_ice_rRng_width = getBlConfig(RASTER_TUNE_ICE_RING_WIDTH, beamline)
+dials_tune_ice_ring_width = getBlConfig(RASTER_TUNE_ICE_RING_WIDTH, beamline)
 dials_tune_min_spot_size = getBlConfig("rasterDefaultMinSpotSize", beamline)
 dials_tune_thresh_kern =  getBlConfig("rasterThreshKernSize", beamline)
 dials_tune_thresh_sig_bck =  getBlConfig("rasterThreshSigBckrnd", beamline)
 dials_tune_thresh_sig_strong =  getBlConfig("rasterThreshSigStrong", beamline)
 dials_comm_with_params = []
-if (dials_tuneIceRingFlag):
+if (dials_tune_ice_ring_flag):
     dials_comm_with_params.append("ice_rings.filter=true")
-    dials_comm_with_params.append(f"ice_rings.width={dials_tuneIceRingWidth}")
-if (dials_tuneThreshFlag):
-    dials_comm_with_params.append(f"spotfinder.threshold.xds.kernel_size={dials_tuneThreshKern},{dials_tuneThreshKern}")
-    dials_comm_with_params.append(f"spotfinder.threshold.xds.sigma_background={dials_tuneThreshSigBck}")
-    dials_comm_with_params.append(f"spotfinder.threshold.xds.sigma_strong={dials_tuneThreshSigStrong}")
-if (dials_tuneResoFlag):
-    dials_comm_with_params.append(f"spotfinder.filter.d_min={dials_tuneHighRes}")
-    dials_comm_with_params.append(f"spotfinder.filter.d_max={dials_tuneLowRes}")
+    dials_comm_with_params.append(f"ice_rings.width={dials_tune_ice_ring_width}")
+if (dials_tune_thresh_flag):
+    dials_comm_with_params.append(f"spotfinder.threshold.xds.kernel_size={dials_tune_thresh_kern},{dials_tune_thresh_kern}")
+    dials_comm_with_params.append(f"spotfinder.threshold.xds.sigma_background={dials_tune_thresh_sig_bck}")
+    dials_comm_with_params.append(f"spotfinder.threshold.xds.sigma_strong={dials_tune_thresh_sig_strong}")
+if (dials_tune_reso_flag):
+    dials_comm_with_params.append(f"spotfinder.filter.d_min={dials_tune_high_res}")
+    dials_comm_with_params.append(f"spotfinder.filter.d_max={dials_tune_low_res}")
 if beamline == "amx":
-    dials_comm_with_params.append(f"min_spot_size={dials_tuneMinSpotSize}")
-full_dials_command = ' '.join(dials_comm_with_params)
+    dials_comm_with_params.append(f"min_spot_size={dials_tune_min_spot_size}")
+full_dials_command = f'{dials_comm} {" ".join(dials_comm_with_params)}'
 print('dials spotfinder command: %s' % dials_comm_with_params) 
 
-request = db_lib.getRequestByID(collection_id, active_only)
 directory = request["request_obj"]["directory"]
+file_prefix = request["request_obj"]["file_prefix"]
 if seqNum>1:
     CBF_pattern = os.path.join(directory, 'cbf', f'{file_prefix}_{seqNum}_*.cbf')
 else:
     CBF_pattern = os.path.join(directory, 'cbf', f'prefix_{row_index}_*.cbf')
-request = db_lib.getRequestByID(collection_id, active_only)
-directory = request["request_obj"]["directory"]
 
 comm_s = f"ls {CBF_pattern} | {full_dials_command}"
 os.system(comm_s)
