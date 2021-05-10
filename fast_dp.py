5d7ca2b7cf34c54a4f5554354da42a2382c9a7d9
Jun Aishima <jaishima@bnl.gov>
2021-05-10 13:24:06 -0400

    fixes from beamline tests
     * get beamline from correct level
     * number of default arguments is 2

--- fast_dp.py
+++ fast_dp.py
@@ -5,13 +5,13 @@ import db_lib
 from daq_utils import getBlConfig
 
 collection_id, seq_num = sys.argv[1:3]
-if len(sys.argv) > 3:
+if len(sys.argv) > 2:
     only_active_proc = False
 else:
     only_active_proc = True
 
 result = db_lib.getRequestByID(collection_id, only_active_proc)
-beamline = result['beamline']
+beamline = result["request_obj"]['beamline']
 directory = result["request_obj"]["directory"]
 running_dir = os.path.join(directory, 'fastDPOutput')
 file_prefix = result["request_obj"]["file_prefix"]
