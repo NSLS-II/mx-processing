40b2f7c8e95048155421e23c46001828eb199351
Jun Aishima <jaishima@bnl.gov>
2021-05-10 13:53:20 -0400

    add full path for python scripts

--- edna.sh
+++ edna.sh
@@ -4,4 +4,4 @@ export PYTHONPATH=${PROJDIR}/lsdc:$PYTHONPATH
 export WRAPPERSDIR=${PROJDIR}/wrappers/
 export MONGODB_HOST='xf17id1-lsdcmongo.nsls2.bnl.local'
 export BEAMLINE_ID='fmx' # TODO required to keep daq_utils happy
-/opt/conda_envs/mx-processing-env/bin/python edna.py "$@"
+/opt/conda_envs/mx-processing-env/bin/python ${PROJDIR}/mx-processing/edna.py "$@"
