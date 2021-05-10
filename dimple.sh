7c54763beb5b129abe2a66316d0b057e86a6542b
Jun Aishima <jaishima@bnl.gov>
2021-05-10 13:52:09 -0400

    add FQDN for mongodb host

--- dimple.sh
+++ dimple.sh
@@ -2,5 +2,6 @@
 export PROJDIR=/GPFS/CENTRAL/xf17id1/skinnerProjectsBackup
 export PYTHONPATH=${PROJDIR}/lsdc:$PYTHONPATH
 export WRAPPERSDIR=${PROJDIR}/wrappers/
+export MONGODB_HOST='xf17id1-lsdcmongo.nsls2.bnl.local'
 export BEAMLINE_ID='fmx' # TODO required to keep daq_utils happy
 /opt/conda_envs/mx-processing-env/bin/python dimple.py "$@"
