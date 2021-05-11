f97970c2b1f3003d3709ab0c39ea0c22b72cf16c
Jun Aishima <jaishima@bnl.gov>
2021-05-11 13:19:24 -0400

    fix parameter error, print called command

--- dozor.py
+++ dozor.py
@@ -20,5 +20,6 @@ request = db_lib.getRequestByID(collection_id, active_only)
 directory = request["request_obj"]["directory"]
 dozor_dir = os.path.join(directory, 'dozor', f'row_{row_index}')
 dozor_comm = "/usr/local/crys-local/dozor-10Aug2020/dozor"
-comm_s = f"cd {dozor_dir}; {dozor_comm} -w bin 1 h5_row_{row_index}.dat"
+comm_s = f"cd {dozor_dir}; {dozor_comm} -w -bin 1 h5_row_{row_index}.dat"
+print(f'dozor call: {comm_s}')
 os.system(comm_s)
