27318c7b47d757224e548f3247ca9c6f729009c8
Jun Aishima <jaishima@bnl.gov>
2021-10-19 16:06:40 -0400

    main extraction

--- config_sample.sh
+++ config_sample.sh
@@ -1,2 +1,8 @@
 export ENV_PATH="/opt/conda_envs/mx-processing-env"
+export PROJDIR=/GPFS/xf17id2/skinnerProjectsBackup/
+export LSDCDIR=${PROJDIR}/lsdc
 export PROCESSINGDIR=${PROJDIR}/mx-processing
+export PYTHONPATH=${LSDCDIR}:$PYTHONPATH
+export WRAPPERSDIR=${PROJDIR}/wrappers/
+export MONGODB_HOST='xf17id2-lsdcmongo'
+export BEAMLINE_ID='fmx' # TODO required to keep daq_utils happy
