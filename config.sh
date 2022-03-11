9a85aafa510b3355bd23ea91ace827712ecb81a2
Jun Aishima <jaishima@bnl.gov>
2022-03-11 15:17:07 -0500

    fix env name
     * the name was correct, so during processing, the bash
       script was attempting to run the python script in the
       wrong place

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 export ENV_PATH="/opt/conda_envs/lsdc-processing-2022-1.0"
 export PROJDIR=/GPFS/CENTRAL/xf17id2/skinnerProjectsBackup/
 export LSDCDIR=${PROJDIR}/lsdc
-export PROCESSINGDIR=${PROJDIR}/lsdc-processing
+export PROCESSINGSCRIPTSDIR=${PROJDIR}/lsdc-processing
 export PYTHONPATH=${LSDCDIR}:$PYTHONPATH
 export WRAPPERSDIR=${PROJDIR}/wrappers/
 export MONGODB_HOST='xf17id2-lsdcmongo'
